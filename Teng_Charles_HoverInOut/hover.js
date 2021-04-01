67b8186196a3ca0e139b61aa8e74072543c8b4e8
C-H-Teng <charles.h.teng@gmail.com>
2021-03-31 18:30:09 -0700

    hover in and out assignment

---
+++ Teng_Charles_HoverInOut/hover.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+    $("#zero img").hover(function(){
+        $(this).attr("src","./img/cat0.png");
+    }, function(){
+        $(this).attr("src","./img/ninja0.png");
+    });
+
+    $("#one img").hover(function(){
+        $(this).attr("src","./img/cat1.png");
+    }, function(){
+        $(this).attr("src","./img/ninja1.png");
+    });
+
+    $("#two img").hover(function(){
+        $(this).attr("src","./img/cat2.png");
+    }, function(){
+        $(this).attr("src","./img/ninja2.png");
+    });
+
+    $("#three img").hover(function(){
+        $(this).attr("src","./img/cat3.png");
+    }, function(){
+        $(this).attr("src","./img/ninja3.png");
+    });
+
+    $("#four img").hover(function(){
+        $(this).attr("src","./img/cat4.png");
+    }, function(){
+        $(this).attr("src","./img/ninja4.png");
+    });
+
+})
