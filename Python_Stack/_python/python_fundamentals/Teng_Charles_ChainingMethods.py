be081932ffe0037e4afa83df9271e82c10d86bbd
C-H-Teng <charles.h.teng@gmail.com>
2021-04-05 22:58:05 -0700

    corrected spelling error in User, added Chaining Methods assignment

--- Python_Stack/_python/python_fundamentals/Teng_Charles_ChainingMethods.py
+++ Python_Stack/_python/python_fundamentals/Teng_Charles_ChainingMethods.py
@@ -12,14 +12,17 @@ class user:
 
     def deposit(self, amt):
         self.acct_bal += amt
+        return self
     #end of deposit def
 
     def withdraw(self, amt):
         self.acct_bal -= amt
+        return self
     #end of withdraw def
 
     def display_user_bal(self):
         print(str(self.name)+"'s account balance:", self.acct_bal)
+        return self
     #end of display user balance def
 
     def transfer_bal(self, target, amt):
@@ -27,6 +30,7 @@ class user:
         target.acct_bal += amt
         print(str(self.name)+"'s account balance:", self.acct_bal)
         print(str(target.name)+"'s account balance:", target.acct_bal)
+        return self
     #end of transfer bal def
 #end of user class
 
@@ -36,25 +40,13 @@ charles = user("Charles Teng")
 ying = user("Ying Vuong")
 
 # Have the first user make 3 deposits and 1 withdrawal and then display their balance
-bob.deposit(1000)
-bob.deposit(2000)
-bob.deposit(500) #total: 3500
-bob.withdraw(300) #total: 3200
-bob.display_user_bal()
+bob.deposit(1000).deposit(2000).deposit(500).withdraw(300).display_user_bal()
 
 # Have the second user make 2 deposits and 2 withdrawals and then display their balance
-charles.deposit(5000)
-charles.deposit(2000) #total: 7000
-charles.withdraw(1000)
-charles.withdraw(1500) #total: 4500
-charles.display_user_bal()
+charles.deposit(5000).deposit(2000).withdraw(1000).withdraw(1500).display_user_bal()
 
 # Have the third user make 1 deposits and 3 withdrawals and then display their balance
-ying.deposit(4000)
-ying.withdraw(500) #total: 4500
-ying.withdraw(1000)
-ying.withdraw(250) #total: 2250
-ying.display_user_bal()
+ying.deposit(4000).withdraw(500).withdraw(1000).withdraw(250).display_user_bal()
 
 # BONUS: Add a transfer_money method; have the first user transfer money to the third user and then print both users' balances
 print("Transferring balance...")
