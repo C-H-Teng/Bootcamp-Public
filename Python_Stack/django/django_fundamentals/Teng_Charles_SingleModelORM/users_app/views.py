44a07477fa71d50e51862369d09d94299bf5b0e8
C-H-Teng <charles.h.teng@gmail.com>
2021-04-14 15:59:24 -0700

    Users with templates assignment

--- Python_Stack/django/django_fundamentals/Teng_Charles_SingleModelORM/users_app/views.py
+++ Python_Stack/django/django_fundamentals/Teng_Charles_SingleModelORM/users_app/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render, HttpResponse, redirect
+from .models import Users
 
 def index(request):
-    return HttpResponse("this is the equivalent of @app.route('/')!")
+    context = {
+        "users_list": Users.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def add_user(request):
+    Users.objects.create(
+        first_name=request.POST["fname"],
+        last_name=request.POST["lname"],
+        email_address=request.POST["email"],
+        age=request.POST["age"]
+    )
+    return redirect("/")
