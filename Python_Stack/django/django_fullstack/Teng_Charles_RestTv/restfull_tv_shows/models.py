f1a51354cc3ea4fe72239884359dae31d3953888
C-H-Teng <charles.h.teng@gmail.com>
2021-04-19 21:57:21 -0700

    Shows with validation

--- Python_Stack/django/django_fullstack/Teng_Charles_RestTv/restfull_tv_shows/models.py
+++ Python_Stack/django/django_fullstack/Teng_Charles_RestTv/restfull_tv_shows/models.py
@@ -1,6 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class ShowManager(models.Manager):
+    #validator for creating a show
+    def new_validator(self, postData, shows_list):
+        errors = {}
+        if (len(postData["n_title"]) < 1):
+            errors["n_title"] = "Title of the show must be entered"
+        if (len(postData["n_network"]) < 1):
+            errors["n_network"] = "The network the show airs on must be entered"
+        if (len(postData["n_release"]) < 8):
+            errors["n_release"] = "A valid date must be entered for the release date"
+        if (len(postData["n_desc"]) > 0 and len(postData["n_desc"]) < 10):
+            errors["n_desc"] = "Surely you can think of more to say about this show, right?"
+
+        for show in shows_list:
+            if (show.title == postData["n_title"]):
+                errors["n_title"] = "The show already exists in the database"
+        return errors
+    
+    
+    #validator for editing a show
+    def edit_validator(self, postData, shows_list):
+        errors = {}
+        if (len(postData["e_title"]) < 1):
+            errors["e_title"] = "Title of the show must be entered"
+        if (len(postData["e_network"]) < 1):
+            errors["e_network"] = "The network the show airs on must be entered"
+        if (len(postData["e_release"]) < 8):
+            errors["e_release"] = "A valid date must be entered for the release date"
+        if (len(postData["e_desc"]) > 0 and len(postData["e_desc"]) < 10):
+            errors["e_desc"] = "Surely you can think of more to say about this show, right?"
+
+
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=50)
@@ -8,3 +40,4 @@ class Show(models.Model):
     desc = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
--- Python_Stack/django/django_fullstack/Teng_Charles_RestTv/restfull_tv_shows/views.py
+++ Python_Stack/django/django_fullstack/Teng_Charles_RestTv/restfull_tv_shows/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
-from .models import Show
-# from time import gmtime, strftime
+from django.contrib import messages
+from .models import Show, ShowManager
 from datetime import datetime, date
 
 def index(request):
@@ -13,12 +13,24 @@ def shows(request):
     return render(request, "index.html", context)
 
 def new_show(request):
-    return render(request, "new_show.html")
+    current_date = datetime.now()
+    context = {
+        "present": current_date.strftime("%Y-%m-%d")
+    }
+    return render(request, "new_show.html", context)
 
 def new_show_create(request):
-    Show.objects.create(title=request.POST["n_title"], network=request.POST["n_network"], release=request.POST["n_release"], desc=request.POST["n_desc"])
-    show_id = Show.objects.last().id
-    return redirect(f"/shows/{show_id}/details")
+    errors = Show.objects.new_validator(request.POST, Show.objects.all())
+
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect("/shows/new")
+    
+    else:
+        new_show = Show.objects.create(title=request.POST["n_title"], network=request.POST["n_network"], release=request.POST["n_release"], desc=request.POST["n_desc"])
+    
+    return redirect(f"/shows/{new_show.id}/details")
 
 def show_details(request, show_id):
     context = {
@@ -36,11 +48,19 @@ def show_edit(request, show_id):
 
 def show_update(request, show_id):
     show = Show.objects.get(id=show_id)
-    show.title = request.POST["e_title"]
-    show.network = request.POST["e_network"]
-    show.release = request.POST["e_release"]
-    show.desc = request.POST["e_desc"]
-    show.save()
+    errors = Show.objects.edit_validator(request.POST, Show.objects.all())
+
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f"/shows/{show_id}/edit")
+    
+    else:
+        show.title = request.POST["e_title"]
+        show.network = request.POST["e_network"]
+        show.release = request.POST["e_release"]
+        show.desc = request.POST["e_desc"]
+        show.save()
     return redirect(f"/shows/{show_id}/details")
 
 def delete(request, show_id):
