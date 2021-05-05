846b1bff90f7c91408a3368b5c6109fd236ab604
C-H-Teng <charles.h.teng@gmail.com>
2021-05-05 14:54:07 -0700

    Prop It Up assignment

---
+++ Mern_Stack/teng_charles_propitup/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Person from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      <Person lastName={"Doe"} firstName={"Jane"} age={45} hairColor={"Black"}/>
+      <Person lastName={"Smith"} firstName={"John"} age={88} hairColor={"Brown"}/>
+      <Person lastName={"Fillmore"} firstName={"Millard"} age={50} hairColor={"Brown"}/>
+      <Person lastName={"Smith"} firstName={"Maria"} age={62} hairColor={"Brown"}/>
+    </div> // return div end
+  );
+}
+
+export default App;
